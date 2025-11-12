e0e6011be2d875984e5f0602b61a605747804a6c
jc-gh25 <jeff@jcarl.net>
2025-11-11 17:16:47 -0700

    sixth commit

---
+++ pet-parks/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO amenity (amenity) VALUES('dog friendly');
+INSERT INTO amenity (amenity) VALUES('cat friendly');
+INSERT INTO amenity (amenity) VALUES('turtle friendly');
+INSERT INTO amenity (amenity) VALUES('chicken friendly');
+INSERT INTO amenity (amenity) VALUES('restrooms');
+INSERT INTO amenity (amenity) VALUES('fenced area');
+INSERT INTO amenity (amenity) VALUES('hot dog stand');
+INSERT INTO amenity (amenity) VALUES('parking area');
+INSERT INTO amenity (amenity) VALUES('wifi');
+INSERT INTO amenity (amenity) VALUES('vending machines');
+INSERT INTO amenity (amenity) VALUES('lake');
+INSERT INTO amenity (amenity) VALUES('water fountain');
+INSERT INTO amenity (amenity) VALUES('pay phone');
+INSERT INTO amenity (amenity) VALUES('police substation');
+INSERT INTO amenity (amenity) VALUES('video surveillance');
+INSERT INTO amenity (amenity) VALUES('usb chargers');
